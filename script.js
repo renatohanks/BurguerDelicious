a3309f0a326319344e2c36635f83681718bd62c2
renatohanks <renatohanks@users.noreply.github.com>
2024-09-14 23:08:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -191,7 +191,7 @@ checkoutBtn.addEventListener("click", function () {
 function checkRestaurantOpen() {
   const data = new Date();
   const hora = data.getHours();
-  return hora >= 18 && hora < 23;
+  return hora >= 18 && hora < 00;
 }
 
 const spanItem = document.getElementById("date-span");
